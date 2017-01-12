58743d2a8a31339651de2dc49e1d747d4abe7be7
Mangonels - Dylan Calaf <dylancalaflatham@gmail.com>
2017-01-12 23:21:45 +0100

    Attempt at creating nodes

--- Pacman SERVER/IOManager.cpp
+++ Pacman SERVER/IOManager.cpp
@@ -20,6 +20,7 @@ static void XMLCreateOrSetPlayer(std::string name, int score) {
 	//Simple File IO for generating the player file:
 	ofstream playerscore;
 	playerscore.open(RESOURCE_FILE(name));
+	playerscore << "<?xml version=\"1.0\" encoding=\"UTF - 8\"?>\n";
 	playerscore.close();
 
 	//XML Parse/Edit starts here:
@@ -27,5 +28,7 @@ static void XMLCreateOrSetPlayer(std::string name, int score) {
 	rapidxml::xml_document<> doc; //Creates memory space for XML document
 	doc.parse<0>(xmlFile.data()); //Parses "doc", and assigns the content "data()" from "xmlFile"
 
-//	rapidxml::xml_node<>* allocate_node(node_type node_element, Player);
+	
+	rapidxml::xml_node<> *node = doc.allocate_node(node_type type, const Ch *name = 0, const Ch *value = 0);
+	rapidxml::xml_node<> *node2 = doc.allocate_node(node_element, Player); //(node_type type, const Ch *name=0, const Ch *value=0, std::size_t name_size=0, std::size_t value_size=0)
 }
