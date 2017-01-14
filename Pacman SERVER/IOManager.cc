6945537cd75f1dfa1ef15bda3ca6e170f07c70cb
Mangonels - Dylan Calaf <dylancalaflatham@gmail.com>
2017-01-14 23:13:35 +0100

    aa

--- Pacman SERVER/IOManager.cc
+++ Pacman SERVER/IOManager.cc
@@ -28,7 +28,7 @@ struct Player {
 
 namespace IOManager {
 
-	//Function which recieves the name of a player and the score, and stores them inside an xml file with the same name as the player "player.xml"
+	//Function which recieves the name of a player and the score, and stores them inside an xml file with the same name as the player "player.xml":
 	static void SetPlayerData(Player player) {
 
 		string theScore = to_string(player.score);
@@ -47,47 +47,54 @@ namespace IOManager {
 		xml_document<> xmlDoc; //Creates memory space for XML document
 		xmlDoc.parse<parse_no_data_nodes>(xmlFile.data()); //Parses "doc", and assigns the content "data()" from "xmlFile". With the singularity that we are parsing no nodes (lol).
 
-														   //GENERATING NODES FOR THE VIRTUAL XML DOCUMENT:
-														   //The first node we want to generate is the declaration node, so it should look like this inside the file: <?xml version="1.0" encoding="UTF-8"?>
+		//GENERATING NODES FOR THE VIRTUAL XML DOCUMENT:
+		//The first node we want to generate is the declaration node, so it should look like this inside the file: <?xml version="1.0" encoding="UTF-8"?>
 		xml_node<> *declaration = xmlDoc.allocate_node(node_declaration); //Generates a node, which is allocated inside the xmlDoc. node_declaration is the enum node kind.
 		declaration->append_attribute(xmlDoc.allocate_attribute("version", "1.0")); //Creates an attribute for the previously generated node.
 		declaration->append_attribute(xmlDoc.allocate_attribute("encoding", "utf-8")); //Creates a second attribute.
 		xmlDoc.append_node(declaration); //This actually sets the previously generated node as first child node inside the file. According to documentation.
 
+		//ROOT NODE
 		xml_node<> *root = xmlDoc.allocate_node(node_element, player.name.c_str()); //Generates a node for the playername.
 		root->name(player.name.c_str()); //Sets the name of the node, same as the inserted player name.
 		xmlDoc.append_node(root); //Joins the node to inside of the document.
 
+		//CHILD NODE PLAYERNAME
 		xml_node<>* childName = xmlDoc.allocate_node(node_element, "Name"); //Generates child score node.
 		childName->value(player.name.c_str()); //Sets the value inside this node to the previously parsed to string, int score.
 		root->append_node(childName); //Sets the node inside the root node.
 
+		//CHILD NODE PLAYER SCORE
 		xml_node<>* childScore = xmlDoc.allocate_node(node_element, "Score"); //Generates child score node.
 		childScore->value(theScore.c_str()); //Sets the value inside this node to the previously parsed to string, int score.
 		root->append_node(childScore); //Sets the node inside the root node.
 
-									   //achievement1
+		//CHILD NODE ACHIEVEMENT 1
 		xml_node<>* childAchievement1 = xmlDoc.allocate_node(node_element, "Achievement1"); //Generates child achievements node.
 		childAchievement1->value(theA1.c_str()); //Same as with previous child node.
 		root->append_node(childAchievement1); // ""
-											  //achievement2
+
+		//CHILD NODE ACHIEVEMENT 2
 		xml_node<>* childAchievement2 = xmlDoc.allocate_node(node_element, "Achievement2"); //Generates child achievements node.
 		childAchievement2->value(theA2.c_str()); //Same as with previous child node.
 		root->append_node(childAchievement2); // ""
-											  //achievement3
+		
+		//CHILD NODE ACHIEVEMENT 3
 		xml_node<>* childAchievement3 = xmlDoc.allocate_node(node_element, "Achievement3"); //Generates child achievements node.
 		childAchievement3->value(theA3.c_str()); //Same as with previous child node.
 		root->append_node(childAchievement3); // ""
-											  //achievement4
+		
+		//CHILD NODE ACHIEVEMENT 4
 		xml_node<>* childAchievement4 = xmlDoc.allocate_node(node_element, "Achievement4"); //Generates child achievements node.
 		childAchievement4->value(theA4.c_str()); //Same as with previous child node.
 		root->append_node(childAchievement4); // ""
-											  //achievement5
+		
+		//CHILD NODE ACHIEVEMENT 5
 		xml_node<>* childAchievement5 = xmlDoc.allocate_node(node_element, "Achievement5"); //Generates child achievements node.
 		childAchievement5->value(theA5.c_str()); //Same as with previous child node.
 		root->append_node(childAchievement5); // ""
 
-											  //WRITING THE VIRTUAL XML DOCUMENT ONTO THE FILE, again THROUGH FileIO:
+		//WRITING THE VIRTUAL XML DOCUMENT ONTO THE FILE, again THROUGH FileIO:
 		playerdata.open(RESOURCE_FILE(player.name));
 		playerdata << xmlDoc;
 		playerdata.close();
@@ -96,6 +103,7 @@ namespace IOManager {
 
 	}
 
+	//Function which recieves the name of a player, and extracts a Player struct with all the requested data:
 	static Player GetPlayerData(Player player)
 	{
 		try {
